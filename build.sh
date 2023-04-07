399388889bae1523648549f36936274591353692
liuqiming.lqm <liuqiming.lqm@alibaba-inc.com>
2023-04-07 18:16:29 +0800

    minor changes
    Signed-off-by: liuqiming.lqm <liuqiming.lqm@alibaba-inc.com>

--- build.sh
+++
@@ -1,5 +0,0 @@
-    export LD_LIBRARY_PATH=${LIBPFM4_HOME}/lib:${LD_LIBRARY_PATH}; 
-    export LIBRARY_PATH=${LIBPFM4_HOME}/lib; 
-    export GO_FLAGS="-tags=libpfm,netgo"; 
-    make tidy; 
-    make build -j16;
